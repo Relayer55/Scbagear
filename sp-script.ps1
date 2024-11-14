e4f6a1ae342cbffdce983c19d8364020bc2e57c0
Relayer55 <43385413+Relayer55@users.noreply.github.com>
2024-11-14 07:24:07 -0600

    Initialize 1327-high-risk-service-principal-api-permissions

---
+++ sp-script.ps1
@@ -0,0 +1,114 @@
+Connect-MgGraph -Scopes "Application.Read.All", "Directory.Read.All"
+
+function Initialize-RiskyPermissions {
+    param (
+        [PSCustomObject]$json,
+        [Object[]]$map,
+        [string]$resource,
+        [string]$id
+    )
+
+    $riskyPermissions = $json.permissions.$resource.PSObject.Properties.Name
+    if ($riskyPermissions -contains $id) {
+        $map += $json.permissions.$resource.$id
+    }
+
+    return $map
+}
+
+function Get-ValidCredentials {
+    param(
+        [Array[]]$credentials
+    )
+
+    $validCredentials = @()
+    foreach ($credential in $credentials) {
+        if ($credential.EndDateTime -gt (Get-Date)) { $validCredentials += $credential }
+    }
+    return $validCredentials
+}
+
+$permissionsJson = (Get-Content -Path "./riskyPermissions.json" | ConvertFrom-Json)
+$servicePrincipalResults = @()
+$servicePrincipals = Get-MgBetaServicePrincipal -All
+foreach ($servicePrincipal in $servicePrincipals) {
+    # Exclude Microsoft-published service principals
+    if ($servicePrincipal.AppOwnerOrganizationId -ne "f8cdef31-a31e-4b4a-93e4-5f571e91255a") {
+        $appRoleAssignments = Get-MgBetaServicePrincipalAppRoleAssignment -ServicePrincipalId $servicePrincipal.Id
+        $mappedPermissions = @()
+
+        foreach ($role in $appRoleAssignments) {
+            $resourceDisplayName = $role.ResourceDisplayName
+            $roleId = $role.AppRoleId
+            $mappedPermissions = Initialize-RiskyPermissions -json $permissionsJson -map $mappedPermissions -resource $resourceDisplayName -id $roleId
+        }
+
+        # Disregard entries without risky permissions
+        if ($mappedPermissions.Count -gt 0) {
+            $servicePrincipalResults += [PSCustomObject]@{
+                'Object ID' = $servicePrincipal.Id
+                'App ID' = $servicePrincipal.AppId
+                'Display Name' = $servicePrincipal.DisplayName
+                'Key Credentials' = Get-ValidCredentials -credentials $servicePrincipal.KeyCredentials
+                'Password Credentials' = Get-ValidCredentials -credentials $servicePrincipal.PasswordCredentials
+                'Risky Permissions' = $mappedPermissions
+            }
+        }
+    }
+}
+
+#$servicePrincipalResults = $servicePrincipalResults | Where-Object { $_."Risky Permissions".Count -gt 0 }
+$servicePrincipalResults | ConvertTo-Json -Depth 3 > finalSPResults.json
+
+
+$applications = Get-MgBetaApplication -All
+$applicationResults = @()
+foreach ($app in $applications) {
+
+    # Map permissions assigned to application to risky permissions
+    $mappedPermissions = @()
+    foreach ($resource in $app.RequiredResourceAccess) {
+        # Exclude delegated permissions with property Type="Scope"
+        $roles = $resource.ResourceAccess | Where-Object { $_.Type -eq "Role" }
+        $resourceAppId = $resource.ResourceAppId
+
+        foreach($role in $roles) {
+            $resourceDisplayName = $permissionsJson.resources.$resourceAppId
+            $roleId = $role.Id
+            $mappedPermissions = Initialize-RiskyPermissions -json $permissionsJson -map $mappedPermissions -resource $resourceDisplayName -id $roleId
+        }
+    }
+
+    # Get federated credentials
+    $federatedCredentials = Get-MgBetaApplicationFederatedIdentityCredential -ApplicationId $app.Id -All
+    $federatedCredentialsResults = @()
+
+    # Reformat only if a credential exists
+    if ($null -ne $federatedCredentials) {
+        foreach ($federatedCredential in $federatedCredentials) {
+            $federatedCredentialsResults += [PSCustomObject]@{
+                'Id' = $federatedCredential.Id
+                'Name' = $federatedCredential.Name
+                'Description' = $federatedCredential.Description
+                'Issuer' = $federatedCredential.Issuer
+                'Subject' = $federatedCredential.Subject
+                'Audiences' = $federatedCredential.Audiences | Out-String
+            }
+        }
+    }
+
+    # Disregard entries without risky permissions
+    if ($mappedPermissions.Count -gt 0) {
+        $applicationResults += [PSCustomObject]@{
+            'Object ID' = $app.Id
+            'App ID' = $app.AppId
+            'Display Name' = $app.DisplayName
+            'Key Credentials' = Get-ValidCredentials -credentials $app.KeyCredentials
+            'Password Credentials' = Get-ValidCredentials -credentials $app.PasswordCredentials
+            'Federated Credentials' = $federatedCredentials
+            'Risky Permissions' = $mappedPermissions
+        }
+    }
+}
+
+$applicationResults | ConvertTo-Json -Depth 3 > finalAppResults.json
